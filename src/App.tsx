65c072a223b42ba53a117d0fee7f804bad973744
TamagoGamer <thaleskauan872@gmail.com>
2024-05-31 15:58:01 +0100

    test: :lipstick: Teste de código e formatação

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
+import './app.css';
+import BuildingManager from './buildingmanager';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BuildingManager />
     </div>
   );
 }
---
+++ src/buildingmanager.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './Buildingmanager.css';
+
+function BuildingManager() {
+  const [expandable, setExpandable] = useState(false);
+  const [downsizeable, setDownsizeable] = useState(false);
+
+  return (
+    <div className="building-manager">
+      <h1>Fábrica de construção</h1>
+      <div className="stats">
+        <div className="stat">
+          <h2>Profit</h2>
+          <p>$450</p>
+        </div>
+        <div className="stat">
+          <h2>Manpower</h2>
+          <p>2,405 Total Workers</p>
+        </div>
+        <div className="stat">
+          <h2>Input Materials</h2>
+          <p>10,353 Total Input</p>
+        </div>
+        <div className="stat">
+          <h2>Output Materials</h2>
+          <p>10,353 Total Output</p>
+        </div>
+      </div>
+      <div className="controls">
+        <div className="control">
+          <label>Expandable</label>
+          <input
+            type="checkbox"
+            checked={expandable}
+            onChange={() => setExpandable(!expandable)}
+          />
+        </div>
+        <div className="control">
+          <label>Downsizeable</label>
+          <input
+            type="checkbox"
+            checked={downsizeable}
+            onChange={() => setDownsizeable(!downsizeable)}
+          />
+        </div>
+      </div>
+      <div className="buttons">
+        <button className="erase">Erase All</button>
+        <button className="create">Create</button>
+      </div>
+    </div>
+  );
+}
+
+export default BuildingManager;
