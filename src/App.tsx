1e860b58a6ab9c96722dbd67c8d66f8606736f34
TamagoGamer <thaleskauan872@gmail.com>
2024-06-05 15:40:43 +0100

    test: :sparkles: Para testar o programa

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import BuildingManager from './views/buildingmanager';
 import Header from './components/Header';
 import Goods from './components/Goods';
 import './assets/styles/buildingmanager.css';
+import './assets/styles/themes.css'; // Importe o CSS dos temas
 
 const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark-theme");
+      document.documentElement.classList.remove("light-theme");
+    } else {
+      document.documentElement.classList.add("light-theme");
+      document.documentElement.classList.remove("dark-theme");
+    }
+  }, [darkMode]);
+
   return (
     <Router>
       <div className="app">
-        <Header />
+        <Header darkMode={darkMode} handleToggle={handleToggle} />
         <div className="main-content d-flex">
           <Sidebar />
           <div className="content">
             <Routes>
+              <Route path="/" element={<BuildingManager />} />
               <Route path="/Home" element={<BuildingManager />} />
-              <Route path="/Dashboard" element={<BuildingManager />} />
-              <Route path="/Orders" element={<BuildingManager />} />
-              <Route path="/Products" element={<BuildingManager />} />
-              <Route path="/Customers" element={<BuildingManager />} />
               <Route path="/Goods" element={<Goods />} />
             </Routes>
           </div>
@@ -30,3 +44,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Navbar, Form, FormControl, Button } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
+import "../assets/styles/toggleSwitch.css"; // Importe o CSS
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  darkMode: boolean;
+  handleToggle: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ darkMode, handleToggle }) => {
   return (
     <Navbar bg="light" expand="lg" className="px-3">
       <Navbar.Brand href="/">Building Manager Victoria 3</Navbar.Brand>
@@ -16,6 +22,12 @@ const Header: React.FC = () => {
         <Button variant="outline-success">Search</Button>
       </Form>
       <Button variant="primary" className="ms-2">Export</Button>
+      <label className="ui-switch ms-2">
+        <input type="checkbox" checked={darkMode} onChange={handleToggle} />
+        <div className="slider">
+          <div className="circle"></div>
+        </div>
+      </label>
       <FaUserCircle size={30} className="ms-3" />
     </Navbar>
   );
