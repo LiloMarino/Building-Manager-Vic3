513edabb2c81457842d56a1eafcc6ed9fe565c6a
Murilo <lilo.marino@hotmail.com>
2024-06-07 21:43:33 -0300

    feat: :sparkles: Adicionado espaço para imagem e imagem de exemplo

--- src/views/CreateGoods.tsx
+++ src/views/CreateGoods.tsx
@@ -1,6 +1,6 @@
-import { Component } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import ToggleSwitch from '../components/ToggleSwitch';
+import DDS from "../assets/img/DDS.png";
 
 const CreateGoods: React.FC = () => {
   return (
@@ -8,38 +8,37 @@ const CreateGoods: React.FC = () => {
       <h1 style={{ textDecorationLine: "underline" }}>Goods</h1>
       <Form className="d-flex">
         <Container>
-          <Form.Group>
+          <Form.Group className="mb-3">
             <Form.Label>
               Name<span style={{ color: "red", fontWeight: "bold" }}>*</span>
             </Form.Label>
             <Form.Control></Form.Control>
           </Form.Group>
-          <Form.Group>
+          <Form.Group className="mb-3">
             <Form.Label>
               Cost<span style={{ color: "red", fontWeight: "bold" }}>*</span>
             </Form.Label>
             <Form.Control></Form.Control>
           </Form.Group>
-          <Form.Group>
+          <Form.Group className="mb-3">
             <Form.Label>
               Category
               <span style={{ color: "red", fontWeight: "bold" }}>*</span>
             </Form.Label>
             <Form.Select></Form.Select>
           </Form.Group>
-          <Form.Group>
+          <Form.Group className="mb-3">
             <Form.Label>Consumption tax cost</Form.Label>
             <Form.Control></Form.Control>
           </Form.Group>
-          <Form.Group>
+          <Form.Group className="mb-3">
             <Form.Label>Quantity marketed</Form.Label>
             <Form.Control></Form.Control>
           </Form.Group>
         </Container>
-        <Container className="d-flex flex-column justify-content-evenly">
-          <Form.Group>
-            <Form.Control type="file"></Form.Control>
-          </Form.Group>
+        <Container className="d-flex flex-column justify-content-between">
+          <img src={DDS} alt="DDS File Icon"  className="img-fluid w-25 align-self-center" />
+          <Button variant="secondary">Select File</Button>
           <Form.Group className="container">
             <Row>
               <Col>
